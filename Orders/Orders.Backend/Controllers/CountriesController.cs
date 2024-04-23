6ec7494e060ce7e367abafc49f7bec1a6ea6f436
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-04-23 14:30:37 -0600

    Add basic countries api controllers

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entites;
+
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Metodo para mostrar los registros
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        // Metodo para mostrar los registros con condicion
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        // Metodo para guardar registros
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        // Metodo para eliminar registros
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Metodo para actualizar registros
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -3,15 +3,20 @@ using Orders.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
+
+// Abrir seguridad
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
