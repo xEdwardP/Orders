564abf0a6b23bb28bb3728fc64710adde0cf64e9
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-04-23 13:18:40 -0600

    Create database with the first entity

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entites;
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        //ctor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //prop -> Agregar referencia a Shared
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
