d2a16d722d442782581dfd1b827f9724f1d139aa
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-04-24 18:39:22 -0600

    Begin repository pattern

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7234/") });
+builder.Services.AddScoped<IRepository, Repository>();
+//7128 --> 7234
 
 await builder.Build().RunAsync();
---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace Orders.Frontend.Repositories
+{
+    // Jerarquia en clases
+    // Atributos privados -> Constructores -> Propiedades -> Metodos publicos -> Metodos privados
+
+    // Clase auxiliar para envolver errores
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "RECURSO NO ENCONTRADO!";
+            }
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "TIENES QUE ESTAR LOGUEADO PARA EJECUTAR ESTA OPERACION!";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "NO TIENE PERMISOS PARA REALIZAR ESTA OPERACION!";
+            }
+
+            return "HA OCURRIDO UN ERROR INESPERADO!";
+        }
+    }
+}
