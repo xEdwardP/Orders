6a5fa8fbd4a912889f5764a87d0f9602bdb9f465
xEdwardP <edwardjpineda@outlook.com>
2024-05-05 16:38:06 -0600

    Add Pagination to States Page

--- Orders/Orders.Frontend/Pages/Countries/CountryDetails.razor.cs
+++ Orders/Orders.Frontend/Pages/Countries/CountryDetails.razor.cs
@@ -9,44 +9,89 @@ namespace Orders.Frontend.Pages.Countries
     public partial class CountryDetails
     {
         private Country? country;
-
         [Inject] private NavigationManager NavigationManager { get; set; } = null!;
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
         [Inject] private IRepository Repository { get; set; } = null!;
+        [Parameter] public int CountryId { get; set; }
 
-        [Parameter]
-        public int CountryId { get; set; }
+        //public List<State>? Countries { get; set; }
+        private List<State>? states;
+        private int currentPage = 1;
+        private int totalPages;
 
         protected override async Task OnInitializedAsync()
         {
             await LoadAsync();
         }
 
-        private async Task LoadAsync()
+        private async Task SelectedPageAsync(int page)
+        {
+            currentPage = page;
+            await LoadAsync(page);
+        }
+
+        private async Task LoadAsync(int page = 1)
+        {
+            var ok = await LoadCountryAsync();
+            if (ok)
+            {
+                ok = await LoadStatesAsync(page);
+                if (ok)
+                {
+                    await LoadPagesAsync();
+                }
+            }
+        }
+
+        private async Task LoadPagesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<int>($"api/states/totalPages?id={CountryId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
+                return;
+            }
+            totalPages = responseHttp.Response;
+        }
+
+        private async Task<bool> LoadStatesAsync(int page)
+        {
+            var responseHttp = await Repository.GetAsync<List<State>>($"api/states?id={CountryId}&page={page}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("ERROR!",message, SweetAlertIcon.Error);
+                return false;
+            }
+            states = responseHttp.Response;
+            return true;
+        }
+
+        private async Task<bool> LoadCountryAsync()
         {
             var responseHttp = await Repository.GetAsync<Country>($"/api/countries/{CountryId}");
             if (responseHttp.Error)
             {
-                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                if(responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
                 {
                     NavigationManager.NavigateTo("/countries");
-                    return;
+                    return false;
                 }
-
                 var message = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
-                return;
+                await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
+                return false;
             }
-
             country = responseHttp.Response;
+            return true;
         }
 
         private async Task DeleteAsync(State state)
         {
             var result = await SweetAlertService.FireAsync(new SweetAlertOptions
             {
-                Title = "Confirmación",
-                Text = $"¿Realmente deseas eliminar el departamento/estado? {state.Name}",
+                Title = "CONFIRMACIÓN",
+                Text = $"¿Desea eliminar el departamento/estado? {state.Name}",
                 Icon = SweetAlertIcon.Question,
                 ShowCancelButton = true,
                 CancelButtonText = "No",
@@ -65,7 +110,7 @@ namespace Orders.Frontend.Pages.Countries
                 if (responseHttp.HttpResponseMessage.StatusCode != HttpStatusCode.NotFound)
                 {
                     var message = await responseHttp.GetErrorMessageAsync();
-                    await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                    await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
                     return;
                 }
             }
@@ -78,7 +123,7 @@ namespace Orders.Frontend.Pages.Countries
                 ShowConfirmButton = true,
                 Timer = 3000
             });
-            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con éxito.");
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con éxito!");
         }
     }
 }
