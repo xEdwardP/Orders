7786abaf26c8662b60e7f7d11ae8020a2bec6566
xEdwardP <edwardjpineda@outlook.com>
2024-05-07 15:59:44 -0600

    Create Users repository and Users unit of work

--- Orders/Orders.Backend/Data/SeedDb.cs
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -1,14 +1,18 @@
-﻿using Orders.Shared.Entities;
+﻿using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
+using Orders.Shared.Enums;
 
 namespace Orders.Backend.Data
 {
 	public class SeedDb
 	{
 		private readonly DataContext _context;
+		private readonly IUsersUnitOfWork _usersUnitOfWork;
 
-		public SeedDb(DataContext context)
+		public SeedDb(DataContext context, IUsersUnitOfWork usersUnitOfWork)
         {
 			_context = context;
+			_usersUnitOfWork = usersUnitOfWork;
 		}
 
 		public async Task SeedAsync()
@@ -16,10 +20,45 @@ namespace Orders.Backend.Data
 			await _context.Database.EnsureCreatedAsync();
 			await CheckCountriesAsync();
 			await CheckCategoriesAsync();
+			await CheckRolesAsync();
+			await CheckUserAsync("1010", "Edward", "Pineda", "epineda2022@yopmail.com", "99887766", "Colonia 21", UserType.Admin);
+
 		}
 
-        // Verifica si existen datos en la tabla de Categories
-        private async Task CheckCategoriesAsync()
+		private async Task CheckRolesAsync()
+		{
+			await _usersUnitOfWork.CheckRoleAsync(UserType.Admin.ToString());
+			await _usersUnitOfWork.CheckRoleAsync(UserType.User.ToString());
+		}
+
+		private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, UserType userType)
+		{
+			var user = await _usersUnitOfWork.GetUserAsync(email);
+			if (user == null)
+			{
+				user = new User
+				{
+					FirstName = firstName,
+					LastName = lastName,
+					Email = email,
+					UserName = email,
+					PhoneNumber = phone,
+					Address = address,
+					Document = document,
+					City = _context.Cities.FirstOrDefault(),
+					UserType = userType,
+				};
+
+				await _usersUnitOfWork.AddUserAsync(user, "123456");
+				await _usersUnitOfWork.AddUserToRoleAsync(user, userType.ToString());
+			}
+
+			return user;
+		}
+
+
+		// Verifica si existen datos en la tabla de Categories
+		private async Task CheckCategoriesAsync()
         {
             if (!_context.Categories.Any())
             {
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
 using Orders.Backend.Repositories.Implementations;
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,11 +27,26 @@ builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
 builder.Services.AddScoped<ICitiesRepository, CitiesRepository>();
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 builder.Services.AddScoped<IStatesRepository, StatesRepository>();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 // Injections of Unit of work
 builder.Services.AddScoped<ICategoriesUnitOfWork, CategoriesUnitOfWork>();
 builder.Services.AddScoped<ICitiesUnitOfWork, CitiesUnitOfWork>();
 builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
 builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
+builder.Services.AddScoped<IUsersUnitOfWork, UsersUnitOfWork>();
+
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+	x.User.RequireUniqueEmail = true;
+	x.Password.RequireDigit = false;
+	x.Password.RequiredUniqueChars = 0;
+	x.Password.RequireLowercase = false;
+	x.Password.RequireNonAlphanumeric = false;
+	x.Password.RequireUppercase = false;
+	x.Password.RequiredLength = 6;
+})
+	.AddEntityFrameworkStores<DataContext>()
+	.AddDefaultTokenProviders();
 
 var app = builder.Build();
 SeedData(app);
