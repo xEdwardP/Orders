18e4c0baa437e46dcebac723ad071377c2efab16
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-05-11 13:53:00 -0600

    Modify UsersRepository

--- Orders/Orders.Backend/Repositories/Implementations/UsersRepository.cs
+++ Orders/Orders.Backend/Repositories/Implementations/UsersRepository.cs
@@ -22,7 +22,28 @@ namespace Orders.Backend.Repositories.Implementations
             _signInManager = signInManager;
         }
 
-		public async Task<IdentityResult> AddUserAsync(User user, string password)
+        public async Task<User> GetUserAsync(Guid userId)
+        {
+            var user = await _context.Users
+                .Include(u => u.City!)
+                .ThenInclude(c => c.State!)
+                .ThenInclude(s => s.Country)
+                .FirstOrDefaultAsync(x => x.Id == userId.ToString());
+            return user!;
+        }
+
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
+
+
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
 		{
 			return await _userManager.CreateAsync(user, password);
 		}
