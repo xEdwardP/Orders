ef5ab21e8541b9fee51028b654a6b8f88ecaa3a2
xEdwardP <edwardjpineda@outlook.com>
2024-05-17 15:41:26 -0600

    Created Product Entities

--- Orders/Orders.Shared/Entities/Category.cs
+++ Orders/Orders.Shared/Entities/Category.cs
@@ -5,11 +5,15 @@ namespace Orders.Shared.Entities
 {
 	public class Category : IEntityWithName
     {
-		public int Id { get; set; } // Id del pais
+		public int Id { get; set; }
 
-		[Display(Name = "Categoria")] // Nombre del campo que el usuario ve
-		[MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")] // Mensaje de error
-		[Required(ErrorMessage = "El campo {0} es obligatorio.")] // Si el campo esta vacio
-		public string Name { get; set; } = null!; // Campo nombre
-	}
+		[Display(Name = "Categoria")]
+		[MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string Name { get; set; } = null!;
+
+        public ICollection<ProductCategory>? ProductCategories { get; set; }
+        [Display(Name = "Productos")]
+        public int ProductCategoriesNumber => ProductCategories == null || ProductCategories.Count == 0 ? 0 : ProductCategories.Count;
+    }
 }
