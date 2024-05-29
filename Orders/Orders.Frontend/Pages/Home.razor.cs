f29779d7b1ebf7e4a264116966ca0f324360cb78
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-05-29 10:07:43 -0600

    Revert "Changes to Home"
    This reverts commit b5cd073f4e7a52e696bb3960dc98c7595bdc11f7.

--- Orders/Orders.Frontend/Pages/Home.razor.cs
+++ Orders/Orders.Frontend/Pages/Home.razor.cs
@@ -1,10 +1,6 @@
-﻿using Blazored.Modal.Services;
-using CurrieTechnologies.Razor.SweetAlert2;
+﻿using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Orders.Frontend.Pages.Auth;
 using Orders.Frontend.Repositories;
-using Orders.Shared.DTOs;
 using Orders.Shared.Entities;
 
 namespace Orders.Frontend.Pages
@@ -13,8 +9,6 @@ namespace Orders.Frontend.Pages
     {
         private int currentPage = 1;
         private int totalPages;
-        private int counter = 0;
-        private bool isAuthenticated;
 
         public List<Product>? Products { get; set; }
         [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
@@ -23,81 +17,10 @@ namespace Orders.Frontend.Pages
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
         [Inject] private IRepository Repository { get; set; } = null!;
         [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 8;
-        [CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; } = null!;
-        [CascadingParameter] private IModalService Modal { get; set; } = default!;
-
 
         protected override async Task OnInitializedAsync()
         {
             await LoadAsync();
-        }
-
-        protected async override Task OnParametersSetAsync()
-        {
-            await CheckIsAuthenticatedAsync();
-            await LoadCounterAsync();
-        }
-
-        private async Task CheckIsAuthenticatedAsync()
-        {
-            var authenticationState = await authenticationStateTask;
-            isAuthenticated = authenticationState.User.Identity!.IsAuthenticated;
-        }
-
-        private async Task LoadCounterAsync()
-        {
-            if (!isAuthenticated)
-            {
-                return;
-            }
-
-            var responseHttp = await Repository.GetAsync<int>("/api/temporalOrders/count");
-            if (responseHttp.Error)
-            {
-                return;
-            }
-            counter = responseHttp.Response;
-        }
-
-        private async Task AddToCartAsync(int productId)
-        {
-            if (!isAuthenticated)
-            {
-                Modal.Show<Login>();
-                var toast1 = SweetAlertService.Mixin(new SweetAlertOptions
-                {
-                    Toast = true,
-                    Position = SweetAlertPosition.BottomEnd,
-                    ShowConfirmButton = false,
-                    Timer = 3000
-                });
-                await toast1.FireAsync(icon: SweetAlertIcon.Error, message: "Debes haber iniciado sesión para poder agregar productos al carro de compras!");
-                return;
-            }
-
-            var temporalOrderDTO = new TemporalOrderDTO
-            {
-                ProductId = productId
-            };
-
-            var httpActionResponse = await Repository.PostAsync("/api/temporalOrders/full", temporalOrderDTO);
-            if (httpActionResponse.Error)
-            {
-                var message = await httpActionResponse.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
-                return;
-            }
-
-            await LoadCounterAsync();
-
-            var toast2 = SweetAlertService.Mixin(new SweetAlertOptions
-            {
-                Toast = true,
-                Position = SweetAlertPosition.BottomEnd,
-                ShowConfirmButton = true,
-                Timer = 3000
-            });
-            await toast2.FireAsync(icon: SweetAlertIcon.Success, message: "Producto agregado al carro de compras!");
         }
 
         private async Task SelectedRecordsNumberAsync(int recordsnumber)
@@ -156,7 +79,7 @@ namespace Orders.Frontend.Pages
             if (response.Error)
             {
                 var message = await response.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
                 return false;
             }
             Products = response.Response;
@@ -176,7 +99,7 @@ namespace Orders.Frontend.Pages
             if (response.Error)
             {
                 var message = await response.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("ERROR!", message, SweetAlertIcon.Error);
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
                 return;
             }
             totalPages = response.Response;
@@ -187,6 +110,10 @@ namespace Orders.Frontend.Pages
             int page = 1;
             await LoadAsync(page);
             await SelectedPageAsync(page);
+        }
+
+        private void AddToCartAsync(int productId)
+        {
         }
     }
 }
