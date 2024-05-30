c741e0aad79e299f34ef027ca10417f13ad01798
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-05-30 14:00:11 -0600

    Changes to home

--- Orders/Orders.Frontend/Pages/Home.razor.cs
+++ Orders/Orders.Frontend/Pages/Home.razor.cs
@@ -11,31 +11,51 @@ namespace Orders.Frontend.Pages
 {
     public partial class Home
     {
-        private int currentPage = 1;
-        private int totalPages;
+		private int currentPage = 1;
+		private int totalPages;
 		private int counter = 0;
 		private bool isAuthenticated;
+		private string allCategories = "all_categories_list";
 
 		public List<Product>? Products { get; set; }
-        [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
-        [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
-        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
-        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
-        [Inject] private IRepository Repository { get; set; } = null!;
-        [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 8;
+		public List<Category>? Categories { get; set; }
+		public string CategoryFilter { get; set; } = string.Empty;
+
+
+		[Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
+		[Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
+		[Inject] private NavigationManager NavigationManager { get; set; } = null!;
+		[Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+		[Inject] private IRepository Repository { get; set; } = null!;
+		[Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 8;
+
 		[CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; } = null!;
 		[CascadingParameter] private IModalService Modal { get; set; } = default!;
 
 		protected override async Task OnInitializedAsync()
-        {
-            await LoadAsync();
-        }
+		{
+			await LoadAsync();
+		}
 
 		protected async override Task OnParametersSetAsync()
 		{
 			await CheckIsAuthenticatedAsync();
 			await LoadCounterAsync();
+			await LoadCategoriesAsync();
 		}
+
+		private async Task LoadCategoriesAsync()
+		{
+			var responseHttp = await Repository.GetAsync<List<Category>>("api/categories/combo");
+			if (responseHttp.Error)
+			{
+				var message = await responseHttp.GetErrorMessageAsync();
+				await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+			}
+
+			Categories = responseHttp.Response;
+		}
+
 
 		private async Task CheckIsAuthenticatedAsync()
 		{
@@ -56,6 +76,116 @@ namespace Orders.Frontend.Pages
 				return;
 			}
 			counter = responseHttp.Response;
+		}
+
+		private async Task SelectedRecordsNumberAsync(int recordsnumber)
+		{
+			RecordsNumber = recordsnumber;
+			int page = 1;
+			await LoadAsync(page);
+			await SelectedPageAsync(page);
+		}
+
+		private async Task FilterCallBack(string filter)
+		{
+			Filter = filter;
+			await ApplyFilterAsync();
+			StateHasChanged();
+		}
+
+		private async Task SelectedPageAsync(int page)
+		{
+			currentPage = page;
+			await LoadAsync(page);
+		}
+
+		private async Task LoadAsync(int page = 1, string category = "")
+		{
+			if (!string.IsNullOrWhiteSpace(category))
+			{
+				if (category == allCategories)
+				{
+					CategoryFilter = string.Empty;
+				}
+				else
+				{
+					CategoryFilter = category;
+				}
+			}
+
+			if (!string.IsNullOrWhiteSpace(Page))
+			{
+				page = Convert.ToInt32(Page);
+			}
+
+			var ok = await LoadListAsync(page);
+			if (ok)
+			{
+				await LoadPagesAsync();
+			}
+		}
+
+		private void ValidateRecordsNumber(int recordsnumber)
+		{
+			if (recordsnumber == 0)
+			{
+				RecordsNumber = 8;
+			}
+		}
+
+		private async Task<bool> LoadListAsync(int page)
+		{
+			ValidateRecordsNumber(RecordsNumber);
+			var url = $"api/products?page={page}&RecordsNumber={RecordsNumber}";
+			if (!string.IsNullOrEmpty(Filter))
+			{
+				url += $"&filter={Filter}";
+			}
+
+			if (!string.IsNullOrEmpty(CategoryFilter))
+			{
+				url += $"&CategoryFilter={CategoryFilter}";
+			}
+
+			var response = await Repository.GetAsync<List<Product>>(url);
+			if (response.Error)
+			{
+				var message = await response.GetErrorMessageAsync();
+				await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+				return false;
+			}
+			Products = response.Response;
+			return true;
+		}
+
+		private async Task LoadPagesAsync()
+		{
+			ValidateRecordsNumber(RecordsNumber);
+			var url = $"api/products/totalPages/?RecordsNumber={RecordsNumber}";
+			if (!string.IsNullOrEmpty(Filter))
+			{
+				url += $"&filter={Filter}";
+			}
+			if (!string.IsNullOrEmpty(CategoryFilter))
+			{
+				url += $"&CategoryFilter={CategoryFilter}";
+			}
+
+			var response = await Repository.GetAsync<int>(url);
+			if (response.Error)
+			{
+				var message = await response.GetErrorMessageAsync();
+				await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+				return;
+			}
+			totalPages = response.Response;
+		}
+
+		private async Task ApplyFilterAsync()
+		{
+			int page = 1;
+			await LoadAsync(page);
+			await SelectedPageAsync(page);
 		}
 
 		private async Task AddToCartAsync(int productId)
@@ -98,94 +228,5 @@ namespace Orders.Frontend.Pages
 			});
 			await toast2.FireAsync(icon: SweetAlertIcon.Success, message: "Producto agregado al carro de compras.");
 		}
-
-		private async Task SelectedRecordsNumberAsync(int recordsnumber)
-        {
-            RecordsNumber = recordsnumber;
-            int page = 1;
-            await LoadAsync(page);
-            await SelectedPageAsync(page);
-        }
-
-        private async Task FilterCallBack(string filter)
-        {
-            Filter = filter;
-            await ApplyFilterAsync();
-            StateHasChanged();
-        }
-
-        private async Task SelectedPageAsync(int page)
-        {
-            currentPage = page;
-            await LoadAsync(page);
-        }
-
-        private async Task LoadAsync(int page = 1)
-        {
-            if (!string.IsNullOrWhiteSpace(Page))
-            {
-                page = Convert.ToInt32(Page);
-            }
-
-            var ok = await LoadListAsync(page);
-            if (ok)
-            {
-                await LoadPagesAsync();
-            }
-        }
-
-        private void ValidateRecordsNumber(int recordsnumber)
-        {
-            if (recordsnumber == 0)
-            {
-                RecordsNumber = 8;
-            }
-        }
-
-        private async Task<bool> LoadListAsync(int page)
-        {
-            ValidateRecordsNumber(RecordsNumber);
-            var url = $"api/products?page={page}&RecordsNumber={RecordsNumber}";
-            if (!string.IsNullOrEmpty(Filter))
-            {
-                url += $"&filter={Filter}";
-            }
-
-            var response = await Repository.GetAsync<List<Product>>(url);
-            if (response.Error)
-            {
-                var message = await response.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
-                return false;
-            }
-            Products = response.Response;
-            return true;
-        }
-
-        private async Task LoadPagesAsync()
-        {
-            ValidateRecordsNumber(RecordsNumber);
-            var url = $"api/products/totalPages/?RecordsNumber={RecordsNumber}";
-            if (!string.IsNullOrEmpty(Filter))
-            {
-                url += $"&filter={Filter}";
-            }
-
-            var response = await Repository.GetAsync<int>(url);
-            if (response.Error)
-            {
-                var message = await response.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
-                return;
-            }
-            totalPages = response.Response;
-        }
-
-        private async Task ApplyFilterAsync()
-        {
-            int page = 1;
-            await LoadAsync(page);
-            await SelectedPageAsync(page);
-        }
-    }
+	}
 }
