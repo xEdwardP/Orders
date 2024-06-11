c95e3f43dc5760a7f829c4a120b5019074fe4304
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-06-11 15:51:18 -0600

    Add OrdersDetails and OrdersIndex

--- Orders/Orders.Backend/Controllers/OrdersController.cs
+++ Orders/Orders.Backend/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Orders.Backend.Helpers.Orders;
+using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
 
 namespace Orders.Backend.Controllers
@@ -12,10 +13,12 @@ namespace Orders.Backend.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrdersHelper _ordersHelper;
+        private readonly IOrdersUnitOfWork _ordersUnitOfWork;
 
-        public OrdersController(IOrdersHelper ordersHelper)
+        public OrdersController(IOrdersHelper ordersHelper, IOrdersUnitOfWork ordersUnitOfWork )
         {
             _ordersHelper = ordersHelper;
+            _ordersUnitOfWork = ordersUnitOfWork;
         }
 
         [HttpPost]
@@ -27,6 +30,50 @@ namespace Orders.Backend.Controllers
                 return NoContent();
             }
 
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _ordersUnitOfWork.GetTotalPagesAsync(User.Identity!.Name!, pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var response = await _ordersUnitOfWork.GetAsync(id);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return NotFound(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(OrderDTO orderDTO)
+        {
+            var response = await _ordersUnitOfWork.UpdateFullAsync(User.Identity!.Name!, orderDTO);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
             return BadRequest(response.Message);
         }
     }
